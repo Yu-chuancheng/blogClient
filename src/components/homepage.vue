d52cd2d10ba4185abdf387acd5d041d2a0455c14
yucc <l>
2020-06-03 09:06:24 +0800

    第一次提交

---
+++ src/components/homepage.vue
@@ -0,0 +1,218 @@
+<template>
+  <div id="homepage">
+    <div class="w">
+      <div class="left">
+        <router-view></router-view>
+      </div>
+      <div class="r-content">
+        <div class="allTag">
+          <h4 class="readmore-header">标签分类</h4>
+          <div class="ovh">
+            <router-link
+              v-for="(item,index) in  Labellist"
+              :key="item._id"
+              :to="{path:'/article/findByCategory',query:{articleCategory:item.value}}"
+              :class="'cor'+index%9"
+              exact-active-class="Label"
+            >{{item.name}}</router-link>
+          </div>
+        </div>
+        <div class="readmore-content">
+          <h4 class="readmore-header">专栏</h4>
+          <div class="readmore-title">
+            <div v-for="(item,id) in  serieslist" :key="id" class="series">
+              <router-link
+                class="series-a"
+                exact-active-class="series"
+                :to="{path:'/list',query:{seriesid:item.value}}"
+              >
+                <i class="iconfont">&#xe644;</i>
+                &ensp;{{item.name}}
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <div class="readtime-content">
+          <h4 class="readmore-header">归档</h4>
+          <div class="readtime-items">
+            <router-link
+              exact-active-class="shijiantime"
+              :to="{path:'/list',query:{releasetimes:item}}"
+              v-for="(item,id) in  timelist"
+              :key="id"
+              class="time"
+            >
+              <i class="iconfont"></i>
+              {{item}}
+            </router-link>
+          </div>
+        </div>
+        <div class="readnotice-content">
+          <h4 class="readmore-header">个人信息</h4>
+          <div class="xinxi">
+            <img src="../assets/img/j.jpg" alt />
+            <a
+              href="https://www.jianshu.com/u/0cbdef4e8ecc"
+              target="“_blank”"
+            >&ensp;&ensp;我的简书</a>
+          </div>
+          <div class="xinxi">
+            <img src="../assets/img/qq.png" alt />&ensp;&ensp;1807856155
+          </div>
+          <div class="xinxi">
+            <img src="../assets/img/mail.png" alt /> &ensp;&ensp;1807856155@qq.com
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "homepage",
+  data() {
+    return {
+      Labellist: [],
+      serieslist: [],
+      timelist: []
+    };
+  },
+  methods: {
+    //获取标签的方法
+    getlabel() {
+      this.$axios({
+        method: "get",
+        url: "category/findAllCategory",
+        params: {}
+      }).then(
+        res => {
+          this.Labellist = res.data.result;
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    },
+
+    //获取专栏的方法
+    getcolumn() {
+      this.$axios({
+        method: "get",
+        url: "column/findAllColumn",
+        params: {}
+      }).then(
+        res => {
+          this.serieslist = res.data.result;
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    },
+    //获取时间的方法
+    gettime() {
+      this.$axios({
+        method: "get",
+        url: "/json/article/getDate",
+        params: {}
+      }).then(
+        res => {
+          this.timelist = res.data.data;
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    }
+  },
+  mounted() {
+    this.getlabel();
+    this.getcolumn();
+    // this.gettime();
+  }
+};
+</script>
+<style scoped>
+.xinxi {
+  width: 80%;
+  margin: 0 auto;
+  padding: 8px;
+}
+.xinxi img {
+  height: 20px;
+  vertical-align: middle;
+}
+.w {
+  width: 1200px;
+  margin: 0 auto;
+  overflow: hidden;
+  padding-top: 30px;
+  min-height: px;
+}
+.readtime-content,
+.allTag,
+.readmore-content .readmore-header {
+  margin-top: 14px;
+  margin-bottom: 0px;
+}
+.left {
+  float: left;
+  width: 770px;
+  min-height: 900px;
+}
+.series {
+  width: 90%;
+  height: 25px;
+  margin: 0 auto;
+  font-size: 14px;
+  margin-top: 5px;
+  overflow: hidden; /* 超出的文本隐藏*/
+  text-overflow: ellipsis; /* 溢出用省略号显示*/
+  white-space: nowrap; /*   溢出不换行 */
+}
+.series :hover {
+  color: #fda870;
+}
+.series-a {
+  color: #afafaf;
+}
+.r-content {
+  float: right;
+  margin-top: -10px;
+}
+.router-link {
+  margin-top: -1222px;
+}
+.series {
+  color: #fda870; /*专栏和时间路由的样式*/
+  padding-left: 10px;
+}
+.shijiantime {
+  color: #fda870; /*专栏和时间路由的样式*/
+}
+.Label {
+  background-color: #fda870; /*标签路由的样式*/
+}
+@media screen and (max-width: 800px) {
+  .w {
+    width: 100%;
+    margin: 0 auto;
+    overflow: hidden;
+  }
+  .r-content {
+    float: left;
+    margin-left: -5px;
+    width: 100%;
+    margin-top: 30px;
+  }
+  .left {
+    float: left;
+    width: 100%;
+  }
+  .readtime-items {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+}
+</style>
---
+++ src/components/list.vue
@@ -0,0 +1,327 @@
+<template>
+  <div id="list">
+    <div class="c-list">
+      <router-link
+        tag="div"
+        :to="'/articel/'+item._id"
+        v-for="item in  articlelist"
+        :key="item._id"
+        class="post-content"
+      >
+        <img class="picture" :src="item.image" />
+        <div class="cont">
+          <p>
+            <!-- <em class="recommend" v-if="item.istop==='1'">置顶</em>
+            <em class="tuijian" v-if="item.istop==='0'">推荐</em> -->
+            <span class="content-title">{{item.articleTitle}}</span>
+          </p>
+          <div class="content" v-html="item.articleContent"></div>
+        </div>
+        <div class="post-content-footer">
+          <span class="tag tag-time">{{item.date | dataFormat}}</span>
+          <!-- v-for="(label,index) in item.label" :key="index" -->
+          <span class="post-tag">{{item.articleCategory}}</span>
+          <!-- <span class="tag right ipad" v-if="false">
+            <span>浏览({{item.visits}})</span>
+            <span>留言({{item.comment}})</span>
+          </span> -->
+        </div>
+      </router-link>
+      <!-- <div class="pagepagin-list">
+        <div>
+          <span class="bot_page ye-pc" v-if="!gengduo">
+            <a class="pagef">
+              <i class="iconfont page_icon" @click="more()"></i>更多
+            </a>
+          </span>
+          <span class="bot_page ye">
+            <a href="#" class="pagef">
+              <i class="iconfont page_icon" @click="first()"></i>首页
+            </a>
+          </span>
+          <span
+            v-for="(item,id) in pagelist"
+            :key="id"
+            :class="'page_span_'+id "
+            class="pagination ye"
+          >
+            <a id="page_id3" :class="thispage==item?'pagef':''">
+              {{item}}
+              <i class="iconfont page_icon" @click="getthispagearticlelist(item)"></i>
+            </a>
+          </span>
+          <span class="bot_page ye">
+            <a href="#" class="pagef">
+              <i class="iconfont page_icon" @click="end()"></i>尾页
+            </a>
+          </span>
+        </div>
+      </div> -->
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "list",
+  data() {
+    return {
+      search: "",
+      thisarticletype: "",
+      pagenum: "",
+      pagelist: [],
+      articlelist: [],
+      thispage: "",
+      gengduo: false
+    };
+  },
+  methods: {
+    // get获取所有文章列表
+    getallarticlelist() {
+      this.$axios({
+        method: "GET",
+        url: "article/findAll"
+      }).then(
+        res => {
+          const data = res.data.result;
+          this.articlelist = data;
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    },
+    // get获取【标签】文章列表
+    getLabelarticlelist() {
+      this.$axios({
+        method: "get",
+        url: "/category/findAllCategory"
+      }).then(
+        res => {
+          console.log("标签", res);
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    },
+    // get获取【专栏】文章列表
+    getseriesarticlelist() {
+      this.$axios({
+        method: "post",
+        url: "/json/article/pageQuery",
+        data: this.qs.stringify({
+          columnid: this.seriesid
+        })
+      }).then(
+        res => {
+          //分割字符串
+          let arr = Object.entries(res.data.data);
+          for (var i = 0; i <= arr.length - 1; i++) {
+            res.data.data[i].label = res.data.data[i].label.split(",");
+            res.data.data[i].label = res.data.data[i].label.slice(0, 3);
+          }
+          this.articlelist = res.data.data;
+          this.pagenum = res.data.total;
+          // this.getpagelist();
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    },
+    //点击【更多】获取相应的特定属性文章文章
+    more() {
+     
+    },
+    //点击【首页】获取相应的特定属性文章文章
+    first() {
+      
+    },
+    //点击【尾页】获取相应的特定属性文章文章
+    end() {
+      
+    },
+    //点击某一页获取相应的特定属性文章文章
+    getthispagearticlelist(thispage) {
+      this.thispage = thispage;
+      this.getpagearticlelist(thispage);
+    },
+    // 回到顶部方法，加计时器是为了过渡顺滑
+    backTop() {
+      let that = this;
+      let timer = setInterval(() => {
+        let ispeed = Math.floor(-that.scrollTop / 5);
+        document.documentElement.scrollTop = document.body.scrollTop =
+          that.scrollTop + ispeed;
+        if (that.scrollTop === 0) {
+          clearInterval(timer);
+        }
+      }, 16);
+    }
+  },
+  mounted() {
+    this.getLabelarticlelist();
+    this.getallarticlelist();
+  },
+  //监听
+  watch: {
+    $route(to, from) {
+    }
+  }
+};
+</script>
+<style scoped>
+.tag {
+  color: #000;
+}
+.post-content {
+  overflow: hidden;
+  padding-top: 16px;
+  padding-bottom: 5px;
+  border-radius: 10px;
+  margin: 13px 4px;
+  transition: all 0.28s;
+  box-shadow: 0 6px 12px 0 rgba(0, 0, 0, 0.15);
+  width: 770px;
+  position: relative;
+  height: 156px;
+}
+.post-content:hover {
+  cursor: pointer;
+  opacity: 1;
+  background-color: #ffffff;
+}
+.content-title {
+  font-size: 16px;
+}
+.pagef {
+  color: white;
+}
+p {
+  margin-top: 0px;
+}
+.ye {
+  cursor: pointer;
+}
+.picture {
+  width: 220px;
+  margin-left: 20px;
+}
+.cont {
+  margin-left: 1%;
+  height: 120px;
+  overflow: hidden;
+}
+.cont p {
+  margin-bottom: 0px;
+}
+.content {
+  font-size: 14px;
+  height: 80px;
+}
+.recommend {
+  margin-left: 10px;
+}
+.pagepagin-list {
+  float: left;
+  width: 100%;
+  height: 100px;
+  margin: 0 auto;
+}
+@media screen and (min-width: 800px) {
+  .ye-pc {
+    display: none;
+  }
+  .post-content {
+    margin-top: 0px;
+  }
+  .pagepagin-list {
+    margin-bottom: 30px;
+  }
+  .post-content-footer {
+    margin-top: -26px;
+    min-height: 5px;
+  }
+  .cont {
+    float: right;
+    width: 63%;
+  }
+}
+@media screen and (max-width: 800px) {
+  .ll {
+    position: relative;
+    right: 80px;
+  }
+  .tag {
+    float: left;
+    width: 200px;
+  }
+  .tag-time {
+    margin-left: -250px;
+  }
+  .post-content-footer span {
+    float: left;
+  }
+  .content {
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+  .ye {
+    display: none;
+  }
+  .bot_page {
+    margin-left: 40%;
+  }
+  @media screen and (max-width: 540px) {
+    .picture {
+      display: none;
+    }
+    .post-tag {
+      display: none;
+    }
+    .post-content {
+      width: 98%;
+    }
+    .cont {
+      width: 100%;
+      float: right;
+    }
+    /* .tag-time{
+      margin-top: 123px;
+    } */
+    .ipad {
+      position: absolute;
+      left: 60%;
+      top: 136px;
+      margin-right: 10px;
+    }
+  }
+  @media (min-width: 540px) and (max-width: 800px) {
+    .post-content-footer {
+      margin-top: 1px;
+      min-height: 5px;
+    }
+
+    .ipad {
+      position: absolute;
+      left: 80%;
+      top: 140px;
+      margin-right: 10px;
+    }
+    .post-content .cont {
+      float: right;
+      width: 70%;
+    }
+    .post-content {
+      width: 98%;
+    }
+  }
+  .picture {
+    width: 25%;
+  }
+
+  .post-tag {
+    margin-right: 5%;
+  }
+}
+</style>
